e0bd33e5c5879bd38a3ec77d4f97b9be47f0251e
Sa6rinaa <sabrina.protiere@gmail.com>
2025-04-08 09:43:03 +0200

    exercices

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
+
+
+for ($i=0 ; $i<= 10; $i++) {
+     
+  echo($i);
+}
+
+
+$add=0;
+ for ($i=1 ; $i<= 100; $i++) {
+     $add+=$i;
+     echo($add);
+ }
+
+
 $mult=1;
 for ($i=1 ; $i<= 9; $i++) {
 
     $mult=$mult*$i;
 
     echo($mult);
+
   }
  ?>
 
