e53e0f034bbba04798d682400e00891d3b396266
Sa6rinaa <sabrina.protiere@gmail.com>
2025-04-08 11:38:06 +0200

    exercice_switch

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
 <?php
 
-// Paramètres de connexion à la base de données
-
-
-// DSN (Data Source Name)
-$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-
-// Options pour PDO
-$options = [
-     // Gestion des erreurs avec exceptions
-    // Mode de récupération par défaut: tableau associatif
-    // Désactiver l'émulation des requêtes préparées
-];
-
-
-
-
- ?>
-
-
-
+$jour = date('l');
+ $message = "bonjour";
+ 
+ switch ("") {
+   case "lundi":
+     echo "Aujourd'hui c'est " . $jour . ": " . $message . "<br><br>";
+     break;
+   case "mardi":
+     echo "Aujourd'hui c'est " . $jour . ": " . $message . "<br><br>";
+     break;
+   case "mercredi":
+     echo "Aujourd'hui c'est " . $jour . ": " . $message . "<br><br>";
+     break;
+     case "mercredi":
+       echo "Aujourd'hui c'est " . $jour . ": " . $message . "<br><br>";
+       break;
+     default ;
+     echo "Aujourd'hui c'est " . $jour . ": " . $message . "<br><br>";
+ }
+ 
+ $note=15;
+ 
+ switch (""){
+   case $note<=18 :
+    echo  "Note: " . $note ."/20 - Appréciation:  Excellent (A+)";
+    break;
+    case  $note<=16 :
+    echo "Note: " . $note ."/20 - Appréciation:  Très bien (A)";
+    break;
+    case  $note<=14 :
+     echo "Note: " . $note . "/20 - Appréciation:  Bien";
+    break;
+    case  $note<=12 :
+     echo "Note: " . $note . "/20 - Appréciation: Assez bien";
+     break;
+     default ;
+     echo "Note: " . $note . "/20 - Appréciation: Passable";
+ 
+ 
+ }
