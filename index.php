f4c8899582eb146678ed6bdca72c1c0eff57b612
Sa6rinaa <sabrina.protiere@gmail.com>
2025-04-08 09:34:55 +0200

    Exercice_3

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
-$personne = [
-  "nom" => "Dupont",
-  "prenom" => "Jean",
-  "age" => 30,
-  "ville" => "Paris"
+$fruits = ["pomme", "banane", "orange", "fraise", "kiwi"];
+
+foreach ($fruits as $fruit) {
+  echo "$fruit <br>";}
+
+  $personne = [
+    "nom" => "Dupont",
+    "prenom" => "Jean",
+    "age" => 30,
+    "ville" => "Paris"
+  ];
+
+  foreach ($personne as $personnes) {
+    echo "$personnes <br>";}
+
+$etudiants = [
+    ["nom" => "Martin", "note" => 15],
+    ["nom" => "Durand", "note" => 12],
+    ["nom" => "Petit", "note" => 18]
 ];
 
+foreach ($etudiants as $etudiant) {
+  echo  ("$etudiant[note] <br> "." $etudiant[nom]<br>");}
 
-foreach ($personne as $personnes) {
-  echo "$personnes <br>";}
+
  ?>
 
 
