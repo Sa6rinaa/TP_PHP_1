a53b821e6e502f1c5acd8b391099d4e403d895c7
sa6rinaa <sabrina.protiere@gmail.com>
2025-04-17 15:45:11 +0200

    livre

--- classes/Livre.php
+++ classes/Livre.php
@@ -5,22 +5,60 @@ class Livre {
     protected $auteur;
     protected $anneePublication;
     protected $disponible;
-}
+
 public function __construct($id,$titre,$auteur,$anneePublication, $disponible){
-    
+    $this->id=$id;
+    $this->titre=$titre;
+    $this->auteur=$auteur;
+    $this->anneepublication=$anneePublication;
+    $this->disponible=$disponible;
 }
+
     // Getters
-    
+    public function getId() {
+        return $this->id;
+    }
+    public function getTitre() {
+        return $this->titre;
+    }
+    public function getAuteur() {
+            return $this->auteur;
+        }
+        public function getAnneePublication() {
+            return $this->anneepublication;
+        }
+        public function getDisponible() {
+            return $this->disponible;
+        }
     // Setters
-    
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function setTitre($titre) {
+        $this->titre = $titre;
+    }
+    public function setAuteur($auteur) {
+        $this->auteur = $auteur;
+    }
+    public function setAnneePublication($anneePublication) {
+        $this->anneepublication = $anneePublication;
+    }
+    public function setDisponible($disponible) {
+        $this->disponible = $disponible;
+    }
     // Méthodes
     public function getInfos() {
-    
+    echo " Id : {$this->id} , Titre : {$this->titre} , Auteur : {$this->auteur} , Année de publication : {$this->anneepublication} , Disponible : {$this->disponible} <br>";
     }
     
     public function emprunter() {
+        return "C'est emprunter !";
     }
     
     public function retourner() {
+        return "A retourner";
 }
 
+    }
+
+    
