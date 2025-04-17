90fcb9e8d78ec3bbd22633581588272c85a89443
sa6rinaa <sabrina.protiere@gmail.com>
2025-04-17 15:11:15 +0200

    heritage

--- index.php
+++ index.php
@@ -1,63 +1,82 @@
 <?php
-// Définition d'une classe Personne
-class Personne {
-    // Propriétés (attributs)
-    private $nom;
-    private $prenom;
-    private $age;
+// Classe parente
+class Vehicule {
+    protected $marque;
+    protected $modele;
+    protected $annee;
     
-    public function __construct(string $nom , string $prenom ,int $age){
-      $this->nom=$nom;
-      $this->prenom=$prenom;
-      $this->age=$age;
-  }
-
-    
-    // Getters (accesseurs)
-    
-public function getNom() {
-  return $this->nom;
-}
-public function getPrenom() {
-  return $this->prenom;
-}
-
-public function getAge() {
-  return $this->age;
-}
-
-    // Setters (mutateurs)
-
-    public function setNom($nom){
-      $this->nom= $nom;
-   }
-    public function setPrenom($prenom){
-      $this->prenom= $prenom;
-   }
-   public function setAge($age){
-    $this->age= $age;
- }
-
-    // Méthodes
-
-    // public function sePresenter()
-    public function sePresenter(){
-      echo "Nom : {$this->nom}  ,   Prenom : {$this->prenom} ,  Age : {$this->age}<br>"; 
+    public function __construct($marque,$modele,$annee) {
+      $this->marque=$marque;
+      $this->modele=$modele;
+      $this->annee=$annee;
     }
     
-    // public function estMajeur()
-
-    public function estMajeur() {
-      return $this-> age>= 18;
-  }
+    public function getInfos() {
+        echo "Marque : {$this->marque}  ,  Modele : {$this->modele}  ,    Année : {$this->annee}  ,";
+    }
+    
+    public function demarrer() {
+        return "Le véhicule démarre.";
+    }
 }
-// Utilisation de la classe
-$personne1 = new Personne("Sab", "Prt", 19);
 
-$personne1->sePresenter();
+// Classe enfant qui hérite de Vehicule
+class Voiture extends Vehicule {
+    private $nombrePortes;
+    private $typeCarburant;
+    
+    public function __construct($marque,$modele,$annee,$nombrePortes,$typeCarburant) {
+        // Appel du constructeur parent
+        parent::__construct($marque, $modele, $annee);
+        
+        // Initialisation des propriétés spécifiques
+        $this->nombreportes = $nombrePortes;
+        $this->typecarburant = $typeCarburant;
+    }
+    
+    // Surcharge de la méthode getInfos()
+    public function getInfos() {
+      parent::getInfos();
+      echo "  Nombre de portes :  {$this->nombreportes} , Type de carburant :  {$this->typecarburant} ";
+    }
+    
+    // Méthode spécifique
+    public function klaxonner() {
+        return  "Klaxonne !"; 
+    }
+}
 
-// Modification des propriétés
+// Classe enfant qui hérite de Vehicule
+class Moto extends Vehicule {
+    private $cylindree;
+    
+    public function __construct($marque, $modele, $annee, $cylindree) {
+        parent::__construct($marque, $modele, $annee);
+        $this->cylindree = $cylindree;
+    }
 
-$personne1->setNom("sabrina");
-$personne1->setAge(17);
-$personne1->sePresenter();
+    public function getInfos() {
+        parent::getInfos();
+        echo "Cylindrée : {$this->cylindree} <br>";
+    }
+
+    public function klaxonner() {
+        return "Klaxon de moto !";
+    }
+    
+  
+}
+
+// Utilisation des classes
+$voiture = new Voiture("Renault", "Clio", 2020, 5, "Essence");
+$moto = new Moto("YHA", "777", 2021, "Essence");
+
+$voiture->getInfos();
+
+
+echo "<br>";
+ echo $voiture->klaxonner();
+
+echo "<br>";
+echo $moto->klaxonner();
+
