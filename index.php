545f32d8da14513e44dcb51620f67cf8e538629d
sa6rinaa <sabrina.protiere@gmail.com>
2025-04-17 14:46:38 +0200

    les_classes

--- index.php
+++ index.php
@@ -1,61 +1,63 @@
 <?php
-
-$host = "localhost";
-$dbname = "tp_test";
-$username = "root";
-$password = "";
-$charset = "utf8mb4";
-
-$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false
-];
-
-try {
-    $pdo = new PDO($dsn, $username, $password, $options);
-    echo "Connexion à la base de données réussie ! </br></br>";
-
-
-  
-$sql = "INSERT INTO user (email, name, age) VALUES (?,?,?)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute(["sabrina@gmail.com","Sabrina", 38 ]);
-echo "Nouvel utilisateur inséré avec l'ID: " . $pdo->lastInsertId() . "<br><br>";
-
-
-echo "Utilisateurs de plus de 30 ans:<br>";
-$sql = "SELECT * FROM user WHERE age > ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([30]);
-foreach ($stmt as $stmts) {
-    echo "Nom: " . $stmts["name"] . ", Âge: " . $stmts["age"] . "<br>";
-}
-
-$sql = "UPDATE user SET age = 30 WHERE age = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([38]);
-echo "Nombre d'utilisateurs mis à jour: " . $stmt->rowCount() . "<br>";
-
-$sql = "DELETE FROM user WHERE age = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([38]);
-echo "Nombre d'utilisateurs supprimés: " . $stmt->rowCount() . "<br>";
-
-
-} catch (PDOException $e) {
+// Définition d'une classe Personne
+class Personne {
+    // Propriétés (attributs)
+    private $nom;
+    private $prenom;
+    private $age;
     
-    echo "Erreur de connexion à la base de données: " . $e->getMessage() . "<br>";
+    public function __construct(string $nom , string $prenom ,int $age){
+      $this->nom=$nom;
+      $this->prenom=$prenom;
+      $this->age=$age;
+  }
+
+    
+    // Getters (accesseurs)
+    
+public function getNom() {
+  return $this->nom;
+}
+public function getPrenom() {
+  return $this->prenom;
 }
 
+public function getAge() {
+  return $this->age;
+}
 
+    // Setters (mutateurs)
 
+    public function setNom($nom){
+      $this->nom= $nom;
+   }
+    public function setPrenom($prenom){
+      $this->prenom= $prenom;
+   }
+   public function setAge($age){
+    $this->age= $age;
+ }
 
+    // Méthodes
 
+    // public function sePresenter()
+    public function sePresenter(){
+      echo "Nom : {$this->nom},  Prenom: {$this->prenom}, Age: {$this->age}"; 
+    }
+    
+    // public function estMajeur()
 
+    public function estMajeur() {
+      return $this-> age>= 18;
+  }
+}
+// Utilisation de la classe
+$personne1 = new Personne("Sab", "Prt", 19);
 
+$personne1->sePresenter();
 
+// Modification des propriétés
 
-
+$personne1->setNom("sabrina");
+$personne1->setAge(17);
+$personne1->sePresenter();
